7b29c5f4486945212aaaeb22a365bf5d39030886
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-10-08 04:33:08 +0900

    fix/gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,4 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.code.gson:gson:2.8.9")
-
-    implementation ("com.google.android.material:material:1.10.0")
 }
