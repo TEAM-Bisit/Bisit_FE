6bbe9a59cc810f9fce84ba38a51fc37f1bdede14
answjddn0607 <166084634+answjddn0607@users.noreply.github.com>
2025-11-14 17:06:03 +0900

    Design: 추가 비밀번호 찾기 화면

---
+++ app/src/main/java/com/example/bisit/ui/login/FindPasswordViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.bisit.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FindPasswordViewModel : ViewModel() {
+
+    // 인증번호 입력 UI가 보이는지 여부
+    private val _isVerificationUiVisible = MutableLiveData(false)
+    val isVerificationUiVisible: LiveData<Boolean> get() = _isVerificationUiVisible
+
+    // 휴대폰 인증이 최종 완료되었는지 여부
+    private val _isPhoneVerified = MutableLiveData(false)
+    val isPhoneVerified: LiveData<Boolean> get() = _isPhoneVerified
+
+    val isVerificationUiVisibleInput: MutableLiveData<Boolean> = _isVerificationUiVisible
+    val isPhoneVerifiedInput: MutableLiveData<Boolean> = _isPhoneVerified
+}
